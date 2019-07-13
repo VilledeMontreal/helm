51f2506824c82b81172ad77dab7d4a9a58b23230
Marc Khouzam <marc.khouzam@ville.montreal.qc.ca>
2019-07-13 14:17:47 -0400

    Generalize
    Signed-off-by: Marc Khouzam <marc.khouzam@ville.montreal.qc.ca>

--- scripts/completion-tests/test-completion.sh
+++ scripts/completion-tests/test-completion.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+BINARY_NAME=helm
+BINARY_PATH=_dist/linux-amd64
+
 # Fail as soon as there is an error
 set -e
 
@@ -8,15 +11,15 @@ if [ -z $(which docker) ]; then
   exit 2;
 fi
 
-COMP_DIR=/tmp/helm-tests
+COMP_DIR=/tmp/completion-tests
 COMP_SCRIPT=run-tests.sh
-BASH4_IMAGE=helm-bash4
-BASH3_IMAGE=helm-bash3
-ZSH_IMAGE=helm-zsh
+BASH4_IMAGE=completion-bash4
+BASH3_IMAGE=completion-bash3
+ZSH_IMAGE=completion-zsh
 
 mkdir -p ${COMP_DIR}
 cp scripts/completion-tests/${COMP_SCRIPT} ${COMP_DIR}
-cp _dist/linux-amd64/helm ${COMP_DIR}
+cp ${BINARY_PATH}/${BINARY_NAME} ${COMP_DIR}
 
 ########################################
 # Bash 4 completion tests
@@ -26,7 +29,7 @@ docker build -t ${BASH4_IMAGE} - <<- EOF
    RUN apk update && apk add bash-completion
 EOF
 docker run --rm \
-           -v ${COMP_DIR}:${COMP_DIR} -v ${COMP_DIR}/helm:/bin/helm \
+           -v ${COMP_DIR}:${COMP_DIR} -v ${COMP_DIR}/${BINARY_NAME}:/bin/${BINARY_NAME} \
            ${BASH4_IMAGE} bash -c "source ${COMP_DIR}/${COMP_SCRIPT}"
 
 ########################################
@@ -43,7 +46,7 @@ docker build -t ${BASH3_IMAGE} - <<- EOF
             tar xvz -C /usr/share/bash-completion --strip-components 1 bash-completion-1.3/bash_completion
 EOF
 docker run --rm \
-           -v ${COMP_DIR}:${COMP_DIR} -v ${COMP_DIR}/helm:/bin/helm \
+           -v ${COMP_DIR}:${COMP_DIR} -v ${COMP_DIR}/${BINARY_NAME}:/bin/${BINARY_NAME} \
            -e BASH_COMPLETION=/usr/share/bash-completion \
            ${BASH3_IMAGE} bash -c "source ${COMP_DIR}/${COMP_SCRIPT}"
 
@@ -54,7 +57,7 @@ docker build -t ${ZSH_IMAGE} - <<- EOF
    FROM zshusers/zsh:5.7
 EOF
 docker run --rm \
-           -v ${COMP_DIR}:${COMP_DIR} -v ${COMP_DIR}/helm:/bin/helm \
+           -v ${COMP_DIR}:${COMP_DIR} -v ${COMP_DIR}/${BINARY_NAME}:/bin/${BINARY_NAME} \
            ${ZSH_IMAGE} zsh -c "source ${COMP_DIR}/${COMP_SCRIPT}"
 
 ########################################
@@ -64,11 +67,11 @@ docker run --rm \
 # we run the MacOS tests locally when possible.
 if [ "$(uname)" == "Darwin" ]; then
    # Make sure that for the local tests, the tests will find the newly
-   # built helm.  If for some reason bin/helm is not present and we
-   # don't do this check, the tests may use the default helm installed on
-   # localhost and we won't be testing the right thing.  So we check.
-   if [ $(PATH=$(pwd)/bin:$PATH which helm) != $(pwd)/bin/helm ]; then
-      echo "Cannot find helm under $(pwd)/bin/helm although it is what we need to test."
+   # built binary.  If for some reason the binary is not present under bin/ and we
+   # don't do this check, the tests may use the default binary installed on
+   # localhost and we won't be testing the right thing.  So we check here.
+   if [ $(PATH=$(pwd)/bin:$PATH which ${BINARY_NAME}) != $(pwd)/bin/${BINARY_NAME} ]; then
+      echo "Cannot find ${BINARY_NAME} under $(pwd)/bin/${BINARY_NAME} although it is what we need to test."
       exit 1
    fi
 
